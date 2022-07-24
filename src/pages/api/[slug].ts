6a5bbfbb8038870d3c05a4ffd3dbcb4f8e69f7e2
Gabriel Caixeta <gabrielcaixetaa@gmail.com>
2022-07-24 14:34:42 +0900

    Add link redirect

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const slug = req.query["slug"] as string;
+
+	if (!slug || typeof slug !== "string") {
+		res.status(404).send(JSON.stringify({ error: "No slug provided" }));
+		return;
+	}
+
+	const data = await prisma.shortLink.findFirst({
+		where: { slug: { equals: slug } },
+	});
+	console.log(data);
+
+	if (!data) {
+		res.status(404).send(JSON.stringify({ error: "Slug not found" }));
+	}
+
+	return res.redirect(data.url);
+};
